a312797121daa771d93cd00a6ecc0d41203847fe
Silvana Oliveira <silvana.dev@outlook.com>
2022-07-25 21:30:58 -0300

    silvana-oliveira

---
+++ Readme.md
@@ -0,0 +1,19 @@
+# PORTIFÓLIO DA MINHA TRAJETÓRIA NA REPROGRAMA
+
+## Olá a toda(o)s, sejam bem vindos, esse é meu portifólio com os os projetos desenvolvidos para aquisição de título de Desenvolvedora Front-End pela Reprograma.
+
+Para construção desse portifólio foram usadas habilidades com base conceitual em React e uso de React Hooks.
+
+* onClick
+* onChange
+* Consumo de dados internos no React
+* useState
+* useEffect
+* Consumo de API do GitHub
+
+### Foi uma jornada incrível e chegar até aqui me deixou orgulhosa e com mais sede de conhecimento.
+
+## Obrigado à você que leu até aqui!
+
+## Até a próxima! :)
+
---
+++ src/componentes/Projetos/Dados.js
@@ -0,0 +1,29 @@
+const Dados = [
+    {   
+        title: "Reproburger",
+        gif: "https://media4.giphy.com/media/0FaMaYbvACSww798Bq/giphy.gif",
+        description: "Projeto em HTML5 e CSS3",
+        link: "https://github.com/silvanadev/On17-TodasEmTech-HTML-CSS-I"
+    },
+    {   
+        title: "Webnostalgia",
+        gif: "https://media3.giphy.com/media/reBfWOOSeRVlBX2NQA/giphy.gif?cid=790b7611eb890a92d3121ea7d0aca3d8d11821af28330f0a&rid=giphy.gif&ct=g",
+        description: "Projeto em HTML5 e CSS3 aplicando o conceito de responsividade", 
+        link: "https://github.com/silvanadev/On17-TodasEmTech-HTMLeCSS-II"
+    },
+    {
+        title: "TOP 5 SERIES",
+        gif: "https://media3.giphy.com/media/NKb7cfu9DYmBzSkdar/giphy.gif",
+        description: "Projeto que acessa e retorna dados do objeto",
+        link: "https://github.com/silvanadev/On17-TodasEmTech_JavascriptIII"
+    },
+    {
+        title: "Pesquisa Digimon",
+        gif: "https://media1.giphy.com/media/YfEUYH4ys3thDaVLCn/giphy.gif",
+        description: "Projeto de consumo de API", 
+        link: "https://github.com/silvanadev/On17-TodasEmTech-JavascriptV"
+    }
+
+]
+
+export default Dados
